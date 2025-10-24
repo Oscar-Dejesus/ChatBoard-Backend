3ce75079ec7fb43b7c3302c9c4b930bf8d92d4cc
Oscar Dejesus <oscar6dejesus@gmail.com>
2025-10-24 19:04:12 -0400

    Changed domain for cors frontend

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const port = 5050;
 
 app.use(express.json());
 app.use(cors({
-  origin: 'https://oscar-dejesus.github.io'  
+  origin: 'https://chatboard.online' 
 })); 
 const DBPATH= 'sqlitecloud://cohza82rvz.g6.sqlite.cloud:8860/auth.sqlitecloud?apikey=HrVGdAYYxb7wE0fSAHFbosEULOq2saL8By3K76OMQag';
 const db = new Database(DBPATH);
